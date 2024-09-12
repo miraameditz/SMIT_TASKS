04422fc662d8a44ce50e252442bb6f2106322c61
Muhammad Miram <139574757+miraameditz@users.noreply.github.com>
2024-09-12 22:09:14 +0500

    Add files via upload

---
+++ github tasks/Chapter 01/App.js
@@ -0,0 +1,16 @@
+// answer of question-1
+// alert("welcome to our website");
+
+// answer of question-2
+// alert("Error! Please enter a valid password.");
+
+// answer of question-3
+// alert("Welcome to JS land... \nHappy coding");
+
+// answer of question-4
+// alert("Welcome to JS land...");
+// alert("Happy coding \nPrevent this page from crating additional dialogs.");
+
+// answer of question-5
+// var greet = "Hello... I can run JS through my web  browser's console";
+// alert(greet);
---
+++ github tasks/Chapter 02/App.js
@@ -0,0 +1,41 @@
+//answer of 1
+// var username = "Your Name";
+
+//answer of 2
+// var myName = username; 
+// console.log(myName);
+
+//answer of 3
+// var message = "Hello World";
+// alert(message);
+
+//answer of 4
+// var name = "Your Name";
+// alert(name);
+// var age = "20 Years old";
+// alert(age);
+// var skill = "HTML , CSS , JS";
+// alert(skill);
+
+//answer of 5
+// var food = "Pizza\nPizz\nPiz\nPi\nP";
+// alert(food);
+
+//answer of 6
+// var email = "Yourmail@gmail.com";
+// alert("My email address is " + email)
+
+//answer of 7
+// var book = "A smarter way to learn Javascript";
+// alert("I am trying to learn from the book " + book)
+
+//answer of 8
+// var paragraph = document.querySelector("#head");
+// paragraph.innerHTML = "Yah! I can write HTML content through JavaScript"
+
+//answer of 9
+// var design = "Design Here";
+// alert(design);
+
+// var pattern = document.querySelector("#design");
+// pattern.innerHTML = design;
